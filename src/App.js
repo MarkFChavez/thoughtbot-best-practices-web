f368059bbe9c83c226e95da8f330c763870f552d
Mark Chavez <markchav3z@gmail.com>
2018-11-14 12:14:40 +0800

    Show markdown content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import marked from 'marked';
 import './App.css';
 
+const SOURCE_URL = "https://raw.githubusercontent.com/thoughtbot/guides/master/best-practices/README.md"
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: "" }
+    this.rawMarkup = this.rawMarkup.bind(this)
+  }
+
+  rawMarkup() {
+    let rawMarkup = marked(this.state.text, {sanitize: true})
+    return { __html: rawMarkup }
+  }
+
+  componentWillMount() {
+    fetch(SOURCE_URL)
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        this.setState({ text: text })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
